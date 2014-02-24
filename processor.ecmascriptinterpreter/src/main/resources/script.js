0383420cb07c7d49a5da0973c3b785687b5dcc6a
Christoph Weiß <christoph.weiss@gmail.com>
2014-02-24 16:41:17 +0100

    Finalized scripted prototype

--- processor.ecmascriptinterpreter/src/main/resources/script.js
+++ processor.ecmascriptinterpreter/src/main/resources/script.js
@@ -1,18 +1,9 @@
-function dataInput (in_nb, in_data) 
-{
-	in_data = in_data.concat(" changed");
-};
-
-function eventInput(ev_nb) {
-	eventout[ev_nb].raiseEvent();
-};
-
-
 function clazz(dataout, evout) {
  
 	this.dataInput = function(in_nb, in_data) {
 		in_data = in_data.concat(" changed");
-		output[in_nb].sendData(in_data);
+		str = new java.lang.String(in_data);
+		output[in_nb].sendData(str.getBytes());
 	};
 	
 	this.eventInput = function(ev_nb) {
