70aa251ba6ac745eb40e07153d74516a714dd8c3
Christoph Weiß <christoph.weiss@gmail.com>
2014-02-24 16:23:56 +0100

    Changed script ... currently not working

--- processor.ecmascriptinterpreter/src/main/resources/script.js
+++ processor.ecmascriptinterpreter/src/main/resources/script.js
@@ -1,14 +1,15 @@
-function scriptclass() { // An object literal
-	// the script is provided with the vars:
-	// output:   an array of size 8 representing 8 IRuntimeOutputPorts
-	// eventout: an array of size 8 representing 8 IRuntimeEventTriggererPorts
-	
-	
-	
-	
-	
-	
-	
+function dataInput (in_nb, in_data) 
+{
+	in_data = in_data.concat(" changed");
+};
+
+function eventInput(ev_nb) {
+	eventout[ev_nb].raiseEvent();
+};
+
+
+function clazz(dataout, evout) {
+ 
 	this.dataInput = function(in_nb, in_data) {
 		in_data = in_data.concat(" changed");
 		output[in_nb].sendData(in_data);
@@ -18,3 +19,5 @@ function scriptclass() { // An object literal
 		eventout[ev_nb].raiseEvent();
 	};
 };
+
+var scriptclass = new clazz();
